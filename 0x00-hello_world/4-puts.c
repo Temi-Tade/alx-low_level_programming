dfebc0d6196b3448a3d04e682f18369090aa573c
Temi-Tade <akintadetemi19@gmail.com>
2022-12-08 16:55:51 -0800

    C task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-int main(){
-	puts("Programming is like building a multilingual puzzle");
+int main(void)
+/*
+ * Function that prints something
+ */ 
+{
+	puts("\"Programming is like building a multilingual puzzle");
 	return 0;
 }
