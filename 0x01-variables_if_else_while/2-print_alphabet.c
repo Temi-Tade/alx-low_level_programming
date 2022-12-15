3a393c5664fec238606f10dce472293446581092
Temi-Tade <akintadetemi19@gmail.com>
2022-12-15 10:43:23 -0800

    putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabets with putchar
+ * Return: 0(Success)
+ */
+
+int main(void)
+
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	return (0);
+}
