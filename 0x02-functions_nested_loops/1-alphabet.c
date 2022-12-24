a48e5c70ba03a8ea4250eb44b7e105868e55b37f
Temi-Tade <akintadetemi19@gmail.com>
2022-12-24 10:35:03 -0800

    Task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,6 +1,13 @@
 #include "main.h"
 
-void print_alphabet()
+/**
+ *  * print_alphabet - Prints the alphabet in lowercase.
+ *   *
+ *    * Return: Always 0 (Success)
+ *
+ */
+
+void print_alphabet(void)
 {
 	char a = 'a';
 
@@ -8,5 +15,5 @@ void print_alphabet()
 	{
 		_putchar(a);
 	}
-	_putchar('\n');
+	_putchar('\n'); 
 }
