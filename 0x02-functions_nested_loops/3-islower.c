deddfb9da78daea78722b4cc59418a59147e9985
Temi-Tade <akintadetemi19@gmail.com>
2022-12-26 06:55:06 -0800

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -8,7 +8,8 @@
  *      * @c: The character in ASCII code
  *       *
  *        * Return: 1 for lowercase character. 0 for the rest.
- *         */
+ *
+*/
 int _islower(int c)
 {
 	if (c >= 97 && c <= 122)
